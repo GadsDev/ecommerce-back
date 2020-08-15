5fa3f3fe3d9046adc4525cda4e2a1c5ecdd9f7ea
gustavo.silva <gustavo.silva@codificar.com.br>
2020-08-15 11:47:55 -0300

    Create is Auth and is Admin

--- src/Routes/user.js
+++ src/Routes/user.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-const { requireSignin } = require('../Controllers/AuthController')
+const { requireSignin, isAuth, isAdmin } = require('../Controllers/AuthController')
 const {
     userById
 } = require("../Controllers/UserController")
 
-router.get('/secret/:userId', requireSignin, (req, res) => {
+router.get('/secret/:userId', requireSignin, isAuth, (req, res) => {
     res.json({
         user: req.profile
     })
