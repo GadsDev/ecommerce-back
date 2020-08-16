b117b1bba4713a6cea5ae2b80627c13b477f1b9c
gustavo.silva <gustavo.silva@codificar.com.br>
2020-08-16 11:43:50 -0300

    Find Category by ID

--- src/Controllers/CategoryController.js
+++ src/Controllers/CategoryController.js
@@ -1,6 +1,20 @@
 const Category = require('../Models/Category')
 const { errorHandler } = require('../Helpers/dbErrorHandler')
  
+exports.categoryById = (req, res, next, id) => {
+    Category.findById(id).exec((err, category) => {
+        if (err || !category) {
+           return res.json({error: "Category does not exist"})
+        }
+        req.category = category
+        next()
+    })
+}
+
+exports.read = (req, res) => {
+    return res.json(req.category)
+}
+
 exports.create = (req, res) => {
     const category = new Category(req.body)
     category.save((err, data) => {
--- src/Routes/category.js
+++ src/Routes/category.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
-const { create } = require('../Controllers/CategoryController')
+const { create, categoryById, read } = require('../Controllers/CategoryController')
 const { requireSignin, isAuth, isAdmin } = require('../Controllers/AuthController')
 const { userById } = require("../Controllers/UserController")
 
+router.get('/category/:categoryId', read)
 router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create)
+
 //Middware to userId route param
 router.param('userId', userById)
+router.param('categoryId', categoryById)
 
 module.exports = router
