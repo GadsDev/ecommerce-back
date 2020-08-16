1a99ccde5ebfc8bda285d61449feb45d330f0ac2
gustavo.silva <gustavo.silva@codificar.com.br>
2020-08-16 19:24:09 -0300

    Read And Update User

--- src/Controllers/UserController.js
+++ src/Controllers/UserController.js
@@ -1,6 +1,5 @@
 const User = require('../Models/User')
-const { errorHandler } = require('../Helpers/dbErrorHandler')
- 
+const { errorHandler } = require('../Helpers/dbErrorHandler') 
 
 exports.userById = (req, res, next, id) => {
     User.findById(id).exec((err, user) => {
@@ -8,4 +7,26 @@ exports.userById = (req, res, next, id) => {
         req.profile = user
         next()
    })
+}
+
+exports.read = (req, res) => {
+    req.profile.hashed_password = undefined
+    req.profile.salt = undefined
+    return res.json(req.profile)
+}
+
+exports.update = (req, res) => {
+    User.findOneAndUpdate(
+        { _id: req.profile._id },
+        { $set: req.body },
+        { new: true },
+        (err, user) => {
+            if (err) {
+                return res.status(400).json({ error: 'You are not authorized to this action'})
+            }
+            user.hashed_password = undefined
+            user.salt = undefined
+            return res.json(user)
+        }
+    )
 }
--- src/Routes/user.js
+++ src/Routes/user.js
@@ -2,13 +2,17 @@ const express = require('express')
 const router = express.Router()
 
 const { requireSignin, isAuth, isAdmin } = require('../Controllers/AuthController')
-const { userById } = require("../Controllers/UserController")
+const { userById, read, update } = require("../Controllers/UserController")
+const { route } = require('./product')
 
 router.get('/secret/:userId', requireSignin, isAuth, (req, res) => {
     res.json({
         user: req.profile
     })
 })
+
+router.get('/user/:userId', requireSignin, isAuth, read)
+router.put('/user/:userId', requireSignin, isAuth, update)
 
 //Middware to userId route param
 router.param('userId', userById)
